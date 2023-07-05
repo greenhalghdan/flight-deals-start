09ac485e47aa8284f9bdad825a8ad6679ecde180
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2023-07-05 20:52:11 +0100

    \\\\\\\\\\\\\\\\\\\\\\

--- flight-deals-start/data_manager.py
+++ flight-deals-start/data_manager.py
@@ -1,3 +1,13 @@
+import requests
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+
+    def __init__(self):
+        self.url = "https://api.sheety.co/8af00e58daf0746f6934be397afa9f04/flightDeals/prices"
+        self.header = {
+            "Authorization": "Bearer ksjdh;ksnh;gkljhnsdf;k;"
+        }
+
+    def get_sheet_data(self):
+        response = requests.get(url=self.url, headers=self.header)
+        print(response.json())
--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,1 +1,5 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from flight_data import FlightData
+from data_manager import DataManager
+
+DataManager.get_sheet_data()
